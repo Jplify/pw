2d7b5febcb8407e1fe65253fc8971477722cec0b
Juan Posada <59146925+Jplify@users.noreply.github.com>
2023-03-07 14:48:13 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+    <div id='navbar'>
+    <a href='https://github.com/Jplify?tab=repositories' id='Github' > Github</a>
+    <a href='https://www.linkedin.com/in/juan-posada-b42a8524b/' id='linkedin' > linkedin </a> 
+    <a href='mailto:juanesposada19@gmail.com'> Contact </a>
+    </div>
+    <div id='container'>
+    <h1>Personal Website</h1>
+    <p className = 'text-center text-sm'>
+      Computer Engineer
+    </p>
+
+    </div>
+  {/* <button id='button1'> this is a button </button>  */}
+  </html>
+  );
+}
+
+export default App;
